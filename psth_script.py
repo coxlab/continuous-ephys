dcd9e7283426c4580bdf5ac1e8bd3f6404e3a972
Guitchounts <guitchounts1@Raven.local>
2015-05-05 20:03:44 -0400

    latest ephys code

--- psth_script.py
+++ psth_script.py
@@ -1,9 +1,12 @@
 # This is will be our scratch pad
-
-import mw_utils
-import pixelclock
-import timebase
-
+import sys, os
+sys.path.append('/Volumes/Mac HD/Dropbox (coxlab)/Scripts/Repositories/continuous-ephys/utils')
+sys.path.append('/Volumes/Mac HD/Dropbox (coxlab)/Scripts/Repositories/continuous-ephys/open-ephys analysis/')
+#import mw_utils
+#import pixelclock
+#import timebase
+import OpenEphys
+import matplotlib.pyplot as plt
 
 
 def do_stuff():
@@ -12,6 +15,30 @@ def do_stuff():
 	# 1. read in ephys binary data and timestamps
 
 		# openEPhys code goes here
+datafile = "/Volumes/GG Data Raid/Ephys/2015-04-29_17-35-04_digitalinputtest/100_CH9_4.continuous"
+ephys_data = OpenEphys.load(datafile)
+print "length of data is: ", len(ephys_data['data'])
+print "length of timestamps is: ", len(ephys_data['timestamps'])
+#plt.plot(ephys_data['data'])
+#plt.show()
+#"/Volumes/labuser/Desktop/EPHYS/"
+eventsfile = "/Volumes/GG Data Raid/Ephys/2015-04-29_17-35-04_digitalinputtest/all_channels_3.events"
+events_data = OpenEphys.load(eventsfile)
+# this will be a [#number of events x 1] vector with integer entries; want to turn it into matrix by channel number:
+
+#print "length of digital data is: ", len(events_data['channel'])
+#print "length of digital timestamps is: ", len(events_data['timestamps'])
+plt.plot(events_data['channel'])
+plt.show()
+#print events_data['channel']
+
+
+
+
+
+
+
+
 
 
 	# 2. read in MW pixel clock data
